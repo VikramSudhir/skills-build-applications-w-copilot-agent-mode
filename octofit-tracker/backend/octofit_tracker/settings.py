53d6a7541edefe218c465d10272bbeb7cfd0ccce
VikramSudhir <vikram.sudhindra@accenture.com>
2026-03-12 03:03:25 +0000

    ok

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -28,8 +28,14 @@ SECRET_KEY = 'django-insecure-5vbfgythc=yen-b(ho0)+okdj8b0q(x9@6t&h$3l=0e028olr_
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow localhost and Codespace public URL
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
